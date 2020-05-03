bdf3fcf8053d13ad5dc5f57500db4358c5acaff1
Alexis San Javier <alexis@alexiss-mbp.lan>
2020-05-02 21:00:01 -0400

    added media query to adjust font-size, margins, and paddingsfor screen sizes under 700px

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,27 +6,58 @@ $(document).ready(() => {
 	let stats;
 
 	const trivia = [
-		{
-			question: 'What movie did Thanos first appear in?',
-			choices: ['Avengers 2012', 'Iron Man', 'Captain America', 'Thor'],
-			answer: 'Avengers 2012',
-		},
-		{
-			question: 'What does S.H.I.E.L.D. stand for?',
-			choices: [
-				'Strategic Homeland Intervention Enforcement and Logistics Division',
-				'Strategic Hazard Intervention Espionage Logistics Directorate',
-				'Supreme Headquarters International Espionage Law Enforcement Division',
-				'Supreme Hazard Intervention Enforcement and Logistics Division',
-			],
-			answer:
-				'Strategic Homeland Intervention Enforcement and Logistics Division',
-		},
+	
 		{
 			question: 'How many Infinity Stones exist in the MCU?',
 			choices: ['6', '8', '5', '4'],
 			answer: '6',
 		},
+		{
+			question: 'Captain America\'s shield is made from which metal?',
+			choices: ['Adamantium', 'Strontium', 'Barium', 'Vibranium'],
+			answer: 'Vibranium',
+		},
+		{
+			question: 'What Type of Doctor is Stephen Strange?',
+			choices: ['Neurosurgeon', 'Pediatrician', 'Plastic Surgeon', 'Cardiologist'],
+			answer: 'Neurosurgeon',
+		},
+		{
+			question: 'What Does May Call Spider-Man\'s Heightened Senses?',
+			choices: ['The Tingle', 'Spidey Sense', 'That Special Thing', 'Peter Tingle'],
+			answer: 'Peter Tingle',
+		},
+		{
+			question: 'What Is The Name Of Star-Lord\'s Ship?',
+			choices: ['Walrus', 'Milano', 'Sovereign', 'Simpson'],
+			answer: 'Milano',
+		},
+		{
+			question: 'Who are the shape-shifting aliens in Captain Marvel?',
+			choices: ['Titans', 'Kree', 'Skrulls', 'Xandarians'],
+			answer: 'Skrulls',
+		},
+		{
+			question: 'T\'Challa is the ruler of what nation?',
+			choices: ['Kenya', 'Sokovia', 'Wakanda', 'Nigeria'],
+			answer: 'Wakanda',
+		},
+		{
+			question: 'What radiation turned Bruce Banner into The Hulk?',
+			choices: ['Infrared', 'Gamma', 'X-ray', 'Ultraviolet'],
+			answer: 'Gamma',
+		},
+		{
+			question: 'What particles allow Ant-Man to grow and shrink in size?',
+			choices: ['Iridium', 'Palladium', 'Uru', 'Pym'],
+			answer: 'Pym',
+		},
+		{
+			question: 'Thanos tells Thor, "You should have gone for the ___"?',
+			choices: ['Hand', 'Leg', 'Head', 'Chest'],
+			answer: 'Head',
+		},
+		
 	];
 	
 	const randomize = () => {
@@ -43,8 +74,8 @@ $(document).ready(() => {
 
 		if(time_rem === 0){
 			clearInterval(timer);
-			$('#question').html('Time is Up!')
-			$('#choices').html('The correction answer was: ' + trivia[current].answer);
+			$('#question').text('Time is Up!')
+			$('#choices').html('<div class="result">The correction answer was: <span class="answer">' + trivia[current].answer+'</span></div>');
 			stats.unanswered++;
 			setTimeout(loadQuestion, 2000);
 		}
@@ -61,7 +92,7 @@ $(document).ready(() => {
 			$("#choices").html(Object.entries(stats).map((stat) => {
 				let name = stat[0][0].toUpperCase() + stat[0].slice(1);
 
-				return '<div>' + name + ': ' + stat[1] +'</div>'
+				return '<div class="result">' + name + ': ' + stat[1] +'</div>'
 			}))
 
 			$('#start').show();
@@ -88,11 +119,11 @@ $(document).ready(() => {
 
 		if($(this).text() === trivia[current].answer){
 			stats.correct++;
-			$('#question').html('Correct!')
-			$('#choices').html('The correct answer was: ' + trivia[current].answer);
+			$('#question').text('Correct!')
+			$('#choices').html('<div class="result">The correct answer was: <span class="answer">' + trivia[current].answer + '</span></div>');
 		}else{
-			$('#question').html('Wrong!!')
-			$('#choices').html('The correct answer was: ' + trivia[current].answer);
+			$('#question').text('Wrong!!')
+			$('#choices').html('<div class="result">The correct answer was: <span class="answer">' + trivia[current].answer +'</span></div>');
 			stats.incorrect++;
 		}
 
